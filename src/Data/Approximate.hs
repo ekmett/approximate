c1361db067fa3eab043f4811250e554ca8c53f51
Edward Kmett <ekmett@gmail.com>
2013-04-30 01:42:04 -0400

    Added a Data.Approximate summary module

---
+++ src/Data/Approximate.hs
@@ -0,0 +1,18 @@
+--------------------------------------------------------------------
+-- |
+-- Copyright :  (c) Edward Kmett 2013
+-- License   :  BSD3
+-- Maintainer:  Edward Kmett <ekmett@gmail.com>
+-- Stability :  experimental
+-- Portability: non-portable
+--
+--------------------------------------------------------------------
+module Data.Approximate
+  ( module Data.Approximate.Mass
+  , module Data.Approximate.Numerics
+  , module Data.Approximate.Type
+  ) where
+
+import Data.Approximate.Mass
+import Data.Approximate.Numerics
+import Data.Approximate.Type
